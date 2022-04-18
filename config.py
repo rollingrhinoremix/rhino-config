8d81b2574726bd5b1fa2879c44f967838d3ad813
Sourajyoti Basak <wiz28@protonmail.com>
2022-04-18 20:35:50 +0530

    refactor(config): format code with black

--- config.py
+++ config.py
@@ -4,11 +4,17 @@ import os
 # Change configuration to enable the mainline kernel
 def mainlineInstall():
     os.system("touch ~/.rhino/config/mainline")
-    print("Configuration updated! The mainline kernel will be installed on the next update")
+    print(
+        "Configuration updated! The mainline kernel will be installed on the next update"
+    )
+
 
 # Do not enable the mainline kernel
 def mainlineDenied():
-    print("No changes were made to the Rhino configuration, The mainline kernel will not be installed.")
+    print(
+        "No changes were made to the Rhino configuration, The mainline kernel will not be installed."
+    )
+
 
 # Purge snapd from system and mark it as hold on apt so it will not be reinstalled
 def snapdPurge():
@@ -18,20 +24,28 @@ def snapdPurge():
     os.system("rm -fr ~/snap")
     os.system("sudo apt-mark hold snapd")
     os.system("sudo apt install flatpak gnome-software-plugin-flatpak -y")
-    os.system("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
+    os.system(
+        "flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo"
+    )
     print("Configuration updated, snapd has been removed from the system.")
+
 
 # Do not purge snapd from system
 def snapdKeep():
     print("No changes were made to the Rhino configuration, snapd has not been purged")
+
 
 # Main program
 def config():
     # Splash screen
     print("\nWelcome to the Rhino configuration script")
     print("---")
-    print("The Rhino Configuration script allows for you to customise the rhino-update utility and extend its capabilities, such as allowing for the installation of unsupported software.")
-    print("Please be cautious when using rhino-config, issues can arise from some of the settings so please ensure that you know what you are doing and have read the documentation.")
+    print(
+        "The Rhino Configuration script allows for you to customise the rhino-update utility and extend its capabilities, such as allowing for the installation of unsupported software."
+    )
+    print(
+        "Please be cautious when using rhino-config, issues can arise from some of the settings so please ensure that you know what you are doing and have read the documentation."
+    )
     print("---")
     # Give the user the choice of installing the latest mainline kernel and take user input
     mainline = input("Do you wish to install the latest Linux mainline kernel? [Y/n] ")
@@ -42,15 +56,20 @@ def config():
     elif mainline == "n" or mainline == "N":
         mainlineDenied()
     print("---")
-    snapd = input("Do you wish to remove Snapcraft (snapd) and replace it with Flatpaks? [Y/n]")
+    snapd = input(
+        "Do you wish to remove Snapcraft (snapd) and replace it with Flatpaks? [Y/n]"
+    )
     if snapd == "Y" or snapd == "y" or snapd == "":
         snapdPurge()
     elif snapd == "n" or snapd == "N":
         snapdKeep()
-    
+
     # Print that the program has completed and then exit
-    print("\nrhino-config has completed, please run rhino-update to update your system!")
+    print(
+        "\nrhino-config has completed, please run rhino-update to update your system!"
+    )
     quit()
+
 
 # Call the main program
 config()

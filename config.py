0a6b95b57855387419dde6748281ec0f3b65ea13
Sourajyoti Basak <wiz28@protonmail.com>
2022-04-18 21:26:04 +0530

    refactor(config): improve prompting system

--- config.py
+++ config.py
@@ -1,11 +1,31 @@
 # Imports
 import os
+import sys
 from pathlib import Path
 from shutil import rmtree
 from textwrap import dedent
 
 config_path = Path("~/.rhino/config/").expanduser()
 config_path.mkdir(parents=True, exist_ok=True)
+
+
+def ask(message: str, default_yes: bool = True) -> bool:
+    """
+    Ask the user a question.
+
+    :param message: The message to ask the user.
+    :param default_yes: Whether the default answer should be yes or no.
+    :return: Whether the user answered yes.
+    """
+
+    reply = input(f"{message} {'[Y/n]' if default_yes else '[y/N]'}").strip().upper()
+
+    if reply in ["Y", ""]:
+        return True
+    elif reply == "N":
+        return False
+    else:
+        sys.exit("Unknown reply. Aborting.")
 
 
 def main():
@@ -22,27 +42,19 @@ def main():
         )
     )
 
-    # Give the user the choice of installing the latest mainline kernel and take user input
-    mainline = input("Do you wish to install the latest Linux mainline kernel? [Y/n] ")
-
-    # If user input is yes, go to the mainline install function
-    if mainline == "Y" or mainline == "y" or mainline == "":
+    if ask("Do you wish to install the latest Linux mainline kernel?"):
         (config_path / "mainline").touch(exist_ok=True)
         print(
             "Configuration updated! The mainline kernel will be installed on the next update"
         )
-    # If user input is no, go to the mainline denied function
-    elif mainline == "n" or mainline == "N":
+    else:
         print(
             "No changes were made to the Rhino configuration, The mainline kernel will not be installed."
         )
 
     print("---")
 
-    snapd = input(
-        "Do you wish to remove Snapcraft (snapd) and replace it with Flatpaks? [Y/n]"
-    )
-    if snapd == "Y" or snapd == "y" or snapd == "":
+    if ask("Do you wish to remove Snapcraft (snapd) and replace it with Flatpaks?"):
         (config_path / "snapdpurge").touch(exist_ok=True)
         os.system("sudo rm -rf /var/cache/snapd/")
         os.system("sudo apt autoremove --purge snapd gnome-software-plugin-snap -y")
@@ -53,7 +65,7 @@ def main():
             "flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo"
         )
         print("Configuration updated, snapd has been removed from the system.")
-    elif snapd == "n" or snapd == "N":
+    else:
         print(
             "No changes were made to the Rhino configuration, snapd has not been purged"
         )

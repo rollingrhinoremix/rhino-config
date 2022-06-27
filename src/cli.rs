95bac5a763c4cc3cbb43c59ca7d62f06e1003858
Sourajyoti Basak <wiz28@protonmail.com>
2022-06-27 21:10:12 +0000

    feat: add more xanmod kernel variants

--- src/cli.rs
+++ src/cli.rs
@@ -104,12 +104,12 @@ pub enum SwitchKernel {
 
 #[derive(Subcommand)]
 pub enum EnableKernel {
-    /// Enable the XanMod kernel
+    /// Enable the XanMod kernel. Defaults to the stable variant.
     ///
     /// XanMod is a general-purpose Linux kernel distribution with custom
     /// settings and new features. Built to provide a stable, responsive and
     /// smooth desktop experience
-    Xanmod,
+    Xanmod(XanmodVariants),
 
     /// Enable the Liquorix kernel
     ///
@@ -121,9 +121,36 @@ pub enum EnableKernel {
 
 #[derive(Subcommand)]
 pub enum DisableKernel {
-    /// Disable the XanMod kernel
-    Xanmod,
+    /// Disable the XanMod kernel. Defaults to the stable variant.
+    Xanmod(XanmodVariants),
 
     /// Disable the Liquorix kernel
     Liquorix,
+}
+
+#[derive(Args, PartialEq, Default)]
+pub struct XanmodVariants {
+    /// Stable variant
+    #[clap(short, long)]
+    pub stable: bool,
+
+    /// Mainline variant
+    #[clap(short, long)]
+    pub edge: bool,
+
+    /// Long-Term-Support variant
+    #[clap(short, long)]
+    pub lts: bool,
+
+    /// Realtime variant
+    #[clap(short, long)]
+    pub realtime: bool,
+
+    /// Uses the Task Type CPU scheduler (in development)
+    #[clap(long)]
+    pub tasktype: bool,
+
+    /// Realtime variant of the edge kernel (in development)
+    #[clap(long)]
+    pub realtime_edge: bool,
 }
--- src/commands/switch_kernel.rs
+++ src/commands/switch_kernel.rs
@@ -3,13 +3,67 @@ use std::path::Path;
 
 use anyhow::{ensure, Context, Result};
 
-pub fn enable_xanmod(config_path: &Path) -> Result<()> {
-    ensure!(
-        !config_path.exists(),
-        r#"The XanMod kernel is already enabled! Run "rhino-update" to install it."#
-    );
-    File::create(config_path).context("Failed to create the xanmod config file!")?;
-    println!(r#"The XanMod kernel has been enabled - please run "rhino-update" to install it."#);
+use crate::cli::XanmodVariants;
+
+macro_rules! create_config {
+    ($name: ident, $config_dir: ident) => {{
+        let config_path = $config_dir.join(concat!("xanmod-", stringify!($name)));
+        ensure!(
+            !config_path.exists(),
+            format!(r#"The XanMod kernel variant: `{}` is already enabled! Run "rhino-update" to install it."#, stringify!($name))
+        );
+        File::create(config_path).context(format!("Failed to create the xanmod-{} config file!", stringify!($name)))?;
+        println!("{}", format!(r#"The XanMod kernel variant: `{}` is enabled! Run "rhino-update" to install it."#, stringify!($name)));
+
+    }};
+    ($name: ident, $config_dir: ident, $variants: ident) => {{
+        if $variants.$name {
+            create_config!($name, $config_dir);
+        }
+    }}
+}
+
+macro_rules! remove_config {
+    ($name:ident, $config_dir:ident) => {{
+        let config_path = $config_dir.join(concat!("xanmod-", stringify!($name)));
+        ensure!(
+            config_path.exists(),
+            format!(
+                r#"The XanMod kernel variant: `{}` is already disabled!"#,
+                stringify!($name)
+            )
+        );
+        fs::remove_file(config_path).context(format!(
+            "Failed to remove the xanmod-{} config file!",
+            stringify!($name)
+        ))?;
+        println!(
+            "{}",
+            format!(
+                r#"The XanMod kernel variant: `{}` has been disabled!"#,
+                stringify!($name)
+            )
+        );
+    }};
+    ($name:ident, $config_dir:ident, $variants:ident) => {{
+        if $variants.$name {
+            remove_config!($name, $config_dir);
+        }
+    }};
+}
+
+pub fn enable_xanmod(config_dir: &Path, variants: &XanmodVariants) -> Result<()> {
+    create_config!(stable, config_dir, variants);
+    create_config!(edge, config_dir, variants);
+    create_config!(lts, config_dir, variants);
+    create_config!(realtime, config_dir, variants);
+    create_config!(realtime_edge, config_dir, variants);
+    create_config!(tasktype, config_dir, variants);
+
+    // Default to stable variant if no other variants are specfied
+    if variants == &XanmodVariants::default() {
+        create_config!(stable, config_dir);
+    }
     Ok(())
 }
 
@@ -23,13 +77,18 @@ pub fn enable_liquorix(config_path: &Path) -> Result<()> {
     Ok(())
 }
 
-pub fn disable_xanmod(config_path: &Path) -> Result<()> {
-    ensure!(
-        config_path.exists(),
-        r#"The XanMod kernel is already disabled!"#
-    );
-    fs::remove_file(config_path).context("Failed to remove the xanmod config file!")?;
-    println!(r#"The XanMod kernel has been disabled."#);
+pub fn disable_xanmod(config_dir: &Path, variants: &XanmodVariants) -> Result<()> {
+    remove_config!(stable, config_dir, variants);
+    remove_config!(edge, config_dir, variants);
+    remove_config!(lts, config_dir, variants);
+    remove_config!(realtime, config_dir, variants);
+    remove_config!(realtime_edge, config_dir, variants);
+    remove_config!(tasktype, config_dir, variants);
+
+    // Default to stable variant if no other variants are specfied
+    if variants == &XanmodVariants::default() {
+        remove_config!(stable, config_dir);
+    }
     Ok(())
 }
 
@@ -51,21 +110,38 @@ mod tests {
     use rstest::*;
     use tempfile::{tempdir, TempDir};
 
+    use crate::cli::XanmodVariants;
+
     #[fixture]
     fn temp_dir() -> TempDir { tempdir().unwrap() }
 
     #[rstest]
     fn test_enable_xanmod(temp_dir: TempDir) -> Result<(), Box<dyn Error>> {
-        let config_path = temp_dir.path().join("xanmod");
+        let config_dir = temp_dir.path();
+        let variants = XanmodVariants {
+            stable: true,
+            edge: true,
+            lts: true,
+            realtime: true,
+            realtime_edge: true,
+            tasktype: true,
+        };
 
         // Test that the config file is created
-        super::enable_xanmod(&config_path)?;
-        assert!(config_path.exists());
+        super::enable_xanmod(&config_dir, &variants)?;
+        assert!(config_dir.join("xanmod-stable").exists());
+        assert!(config_dir.join("xanmod-edge").exists());
+        assert!(config_dir.join("xanmod-lts").exists());
+        assert!(config_dir.join("xanmod-realtime").exists());
+        assert!(config_dir.join("xanmod-realtime_edge").exists());
+        assert!(config_dir.join("xanmod-tasktype").exists());
 
         // Test that it errors out if the config file is already present
         assert_eq!(
-            super::enable_xanmod(&config_path).unwrap_err().to_string(),
-            r#"The XanMod kernel is already enabled! Run "rhino-update" to install it."#
+            super::enable_xanmod(&config_dir, &variants)
+                .unwrap_err()
+                .to_string(),
+            r#"The XanMod kernel variant: `stable` is already enabled! Run "rhino-update" to install it."#
         );
 
         Ok(())
@@ -92,17 +168,45 @@ mod tests {
 
     #[rstest]
     fn test_disable_xanmod(temp_dir: TempDir) -> Result<(), Box<dyn Error>> {
-        let config_path = temp_dir.path().join("xanmod");
-        File::create(&config_path)?;
+        let config_dir = temp_dir.path();
+        let variants = XanmodVariants {
+            stable: true,
+            edge: true,
+            lts: true,
+            realtime: true,
+            realtime_edge: true,
+            tasktype: true,
+        };
+
+        let xanmod_stable = &config_dir.join("xanmod-stable");
+        let xanmod_edge = &config_dir.join("xanmod-edge");
+        let xanmod_lts = &config_dir.join("xanmod-lts");
+        let xanmod_realtime = &config_dir.join("xanmod-realtime");
+        let xanmod_realtime_edge = &config_dir.join("xanmod-realtime_edge");
+        let xanmod_tasktype = &config_dir.join("xanmod-tasktype");
+
+        File::create(xanmod_stable)?;
+        File::create(xanmod_edge)?;
+        File::create(xanmod_lts)?;
+        File::create(xanmod_realtime)?;
+        File::create(xanmod_realtime_edge)?;
+        File::create(xanmod_tasktype)?;
 
         // Test that the config file is deleted
-        super::disable_xanmod(&config_path)?;
-        assert!(!config_path.exists());
+        super::disable_xanmod(&config_dir, &variants)?;
+        assert!(!xanmod_stable.exists());
+        assert!(!xanmod_edge.exists());
+        assert!(!xanmod_lts.exists());
+        assert!(!xanmod_realtime.exists());
+        assert!(!xanmod_realtime_edge.exists());
+        assert!(!xanmod_tasktype.exists());
 
         // Test that it errors out if the config file is not present
         assert_eq!(
-            super::disable_xanmod(&config_path).unwrap_err().to_string(),
-            r#"The XanMod kernel is already disabled!"#
+            super::disable_xanmod(&config_dir, &variants)
+                .unwrap_err()
+                .to_string(),
+            r#"The XanMod kernel variant: `stable` is already disabled!"#
         );
         Ok(())
     }
